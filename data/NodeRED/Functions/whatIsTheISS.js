27898d8c5d161bdf875e16ce59b28f42a2c9c897
Joe Pavitt <joepavit@uk.ibm.com>
2018-07-16 10:10:37 +0100

    Remove hardcoded iframe URL

--- data/NodeRED/Functions/whatIsTheISS.js
+++ data/NodeRED/Functions/whatIsTheISS.js
@@ -1,3 +1,3 @@
-msg.text = 'The International Space Station (ISS) is...'
+msg.text = 'The International Space Station (ISS) is a habitable artificial satellite in low Earth orbit (~400km). It has between 3 and 6 astronauts onboard at any giving time and serves as a microgravity and space environment research laboratory.'
 
 return msg;
